8feb3bb3786ebf0e519e8ee3886088ae90e20b62
Lawrence Lam <lawrencl@gmail.com>
2014-11-07 17:54:29 -0500

    comments update

--- Python_Boxplot_pandas_matplotlib.py
+++ Python_Boxplot_pandas_matplotlib.py
@@ -2,19 +2,26 @@
 """
 Created on Thu Nov  6 19:31:38 2014
 
+Homework1 for MIT ESD.756J /15.062 Data Mining 
+
 @author: laml
 """
-
-#problem 3.3a
 import pandas as pd #plot graph
 import matplotlib.pyplot as plt #ylabel
+
 #read file into data2 variable
-file = '/Users/laml/Documents/Fall 2014/ESD.754 Data Mining/HW1/LaptopSalesJanuary2008.txt'
+#file = '/Users/laml/Documents/Fall 2014/ESD.754 Data Mining/HW1/LaptopSalesJanuary2008.txt'
+file = 'testdata.txt'
 data2 = pd.read_csv(file, sep='\t', index_col='Date', parse_dates=True)
-#create pivot table for Retail Price values categorized by Store Postcode
-#plot as bar graph
+
+#3.3a Bar plot pivot table
 #data2.pivot_table(values='Retail Price', index=['Store Postcode']).plot(kind='bar', title="Mean Retail Price per Store Postcode", legend=True)
+
+#testing column logic for specific value
 #print data2[data2['Store Postcode']=='W4 3PH']
+
+#3.3b Boxplot side by side view
 data2[data2['Store Postcode']==('N17 6QA')].append(data2[data2['Store Postcode']==('W4 3PH')]).boxplot(column='Retail Price',by='Store Postcode',rot='90')
+
 #label y
 plt.ylabel("Retail Price")
