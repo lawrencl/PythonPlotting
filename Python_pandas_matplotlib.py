3e7d9fefe28d292df5261f9c38a61f2227872b53
Lawrence Lam <lawrencl@gmail.com>
2014-11-13 11:37:05 -0500

    scatter matrix updates

--- Python_pandas_matplotlib.py
+++ Python_pandas_matplotlib.py
@@ -10,9 +10,14 @@ import pandas as pd #plot graph
 import matplotlib.pyplot as plt #ylabel
 
 #read file into data2 variable
-#file = '/Users/laml/Documents/Fall 2014/ESD.754 Data Mining/HW1/LaptopSalesJanuary2008.txt'
-file = 'testdata.txt'
-data2 = pd.read_csv(file, sep='\t', index_col='Date', parse_dates=True)
+#file1 = '/Users/laml/Documents/Fall 2014/ESD.754 Data Mining/HW1/LaptopSalesJanuary2008.txt'
+#file2 = '/Users/laml/Documents/Fall 2014/ESD.754 Data Mining/HW1/ToyotaCorolla.txt'
+data1 = pd.read_csv(file2, sep='\t')file = 'testdata.txt'
+#data2 = pd.read_csv(file, sep='\t', index_col='Date', parse_dates=True)
+
+#2.11a Matrix plot histogram diagonal
+#pd.tools.plotting.scatter_matrix(data2[['Price','Age_08_04','KM','HP','cc','Weight','Mfr_Guarantee']], alpha=0.2, diagonal='hist')
+
 
 #3.3a Bar plot pivot table
 #data2.pivot_table(values='Retail Price', index=['Store Postcode']).plot(kind='bar', title="Mean Retail Price per Store Postcode", legend=True)
--- scatter_matrix.py
+++ scatter_matrix.py
@@ -3,7 +3,7 @@ import matplotlib.pyplot as plt #ylabel
 
 #read file into data2 variable
 #file = '/Users/laml/Documents/Fall 2014/ESD.754 Data Mining/HW1/Python Github/testdata.txt'
-file = '/Users/laml/Documents/Fall 2014/ESD.754 Data Mining/HW1/ToyotaCorolla.txt'
+file = '/Users/laml/Documents/Fall 2014/ESD.754 Data Mining/HW1/Python Github/ToyotaCorolla.txt'
 data2 = pd.read_csv(file, sep='\t')
 #data2=pd.read_excel(file2, 'data', index_col=None, na_values=['NA'])
 #data2 = pd.read_csv(file, sep='\t', index_col=None, parse_dates=False)
@@ -11,7 +11,7 @@ data2 = pd.read_csv(file, sep='\t')
 #create pivot table for Retail Price values categorized by Store Postcode
 #plot as boxchart graph with specific values
 #data2[data2['Store Postcode']==('N17 6QA')].append(data2[data2['Store Postcode']==('W4 3PH')]).boxplot(column='Retail Price',by='Store Postcode',rot='90')
-pd.tools.plotting.scatter_matrix(data2[['Price','Age_08_04','KM','HP','cc','Weight','Mfr_Guarantee']], alpha=0.2, diagonal='hist').show()
+pd.tools.plotting.scatter_matrix(data2[['Price','Age_08_04','KM','HP','cc','Weight','Mfr_Guarantee']], alpha=0.2, diagonal='hist')
 #pd.tools.plotting.scatter_matrix(data2[['Weight','Height']], alpha=0.2, diagonal='hist')
 
 #label y
